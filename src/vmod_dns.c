394e4fc2b780901a03043c049ed2c5180247e664
Nils Goroll <nils.goroll@uplex.de>
2018-03-16 00:39:00 +0100

    add valid_ip() and valid_host() functions + tests + docs

--- src/vmod_dns.c
+++ src/vmod_dns.c
@@ -6,11 +6,13 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "vdef.h"
 #include "vrt.h"
 #include "miniobj.h"
 #include "vas.h"
+#include "vsa.h"
 
 #include "vcc_dns_if.h"
 
@@ -75,4 +77,95 @@ vmod_rresolve(VRT_CTX, VCL_STRING hostname)
 
 	freeaddrinfo(res);
 	return (p);
+}
+
+#define c4(sa) (&((const struct sockaddr_in *)sa)->sin_addr)
+#define c6(sa) (&((const struct sockaddr_in6 *)sa)->sin6_addr)
+
+static int
+cmp_addr(const struct sockaddr *a, const struct sockaddr *b)
+{
+	AN(a);
+	AN(b);
+	assert(a->sa_family == b->sa_family);
+
+	switch (a->sa_family) {
+	case AF_INET:
+		return (memcmp(c4(a), c4(b), sizeof (struct in_addr)));
+	case AF_INET6:
+		return (memcmp(c6(a), c6(b), sizeof (struct in6_addr)));
+	default:
+		INCOMPL();
+	}
+}
+
+#undef c4
+#undef c6
+
+
+static VCL_STRING
+valid_ip(VRT_CTX, const struct sockaddr *sa, const socklen_t sl)
+{
+	char node[NI_MAXHOST];
+	struct addrinfo hints, *res0 = NULL;
+	const struct addrinfo *res = NULL;
+	const char *r = NULL;
+
+	AN(sa);
+	if (getnameinfo(sa, sl, node, sizeof(node), NULL, 0, NI_NAMEREQD))
+		return (NULL);
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = sa->sa_family;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if (getaddrinfo(node, "80", &hints, &res0))
+		return (NULL);
+
+	for (res = res0; res != NULL; res = res->ai_next) {
+		if (res->ai_family == sa->sa_family &&
+		    res->ai_addrlen == sl &&
+		    cmp_addr(res->ai_addr, sa) == 0) {
+			r = VRT_CollectString(ctx, node, vrt_magic_string_end);
+			break;
+		}
+	}
+	freeaddrinfo(res0);
+	return (r);
+}
+
+VCL_STRING
+vmod_valid_ip(VRT_CTX, VCL_IP ip)
+{
+	const struct sockaddr *sa;
+	socklen_t sl;
+
+	AN(ip);
+	assert(VSA_Sane(ip));
+
+	sa = VSA_Get_Sockaddr(ip, &sl);
+	return (valid_ip(ctx, sa, sl));
+}
+
+VCL_STRING
+vmod_valid_host(VRT_CTX, VCL_STRING node, VCL_ENUM check)
+{
+	struct addrinfo hints, *res0 = NULL;
+	const struct addrinfo *res = NULL;
+	const char *r = NULL;
+	const int all = (check == vmod_enum_all);
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_socktype = SOCK_STREAM;
+
+	if (getaddrinfo(node, "80", &hints, &res0))
+		return (NULL);
+
+	for (res = res0; res != NULL; res = res->ai_next) {
+		r = valid_ip(ctx, res->ai_addr, res->ai_addrlen);
+		if (all && r == NULL)
+			break;
+	}
+	freeaddrinfo(res0);
+	return (r);
 }
